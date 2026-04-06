e3acbe4d44d2c12a5dfdb1392e3ae1c20a969313
PixelCraftLab <vishalgowdask65@gmail.com>
2026-04-06 18:27:39 +0530

    didtance calculation logic added

--- backend-python/scanner.py
+++ backend-python/scanner.py
@@ -4,6 +4,16 @@ from bleak import BleakScanner
 
 SERVER_URL = "http://localhost:5000/data"
 
+
+def rssi_to_distance(rssi):
+    TX_POWER = -59   
+    n = 2            
+
+    distance = 10 ** ((TX_POWER - rssi) / (10 * n))
+    return distance
+
+
+
 previous_rssi = {}
 
 def detection_callback(device, advertisement_data):
@@ -23,7 +33,10 @@ def detection_callback(device, advertisement_data):
 
     previous_rssi[address] = rssi
 
-    print(f"{name} | RSSI: {rssi} | {movement}")
+    # print(f"{name} | Current RSSI: {rssi} | {movement}")
+    distance = rssi_to_distance(rssi)
+
+    print(f"{name} | RSSI: {rssi} | Distance: {round(distance,2)} | {movement}")
 
 
 async def main():
